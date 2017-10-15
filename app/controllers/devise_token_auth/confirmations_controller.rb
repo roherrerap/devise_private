678b5047bbb654e2e8f2c00041a52a2bd5c723a5
LAPTOP-B91FCI08\Noctis <roherrerap@unal.edu.co>
2017-10-14 19:24:23 -0500

    Initial commit

---
+++ app/controllers/devise_token_auth/confirmations_controller.rb
@@ -0,0 +1,34 @@
+module DeviseTokenAuth
+  class ConfirmationsController < DeviseTokenAuth::ApplicationController
+    def show
+      @resource = resource_class.confirm_by_token(params[:confirmation_token])
+
+      if @resource && @resource.id
+        # create client id
+        client_id  = SecureRandom.urlsafe_base64(nil, false)
+        token      = SecureRandom.urlsafe_base64(nil, false)
+        token_hash = BCrypt::Password.create(token)
+        expiry     = (Time.now + @resource.token_lifespan).to_i
+
+        @resource.tokens[client_id] = {
+          token:  token_hash,
+          expiry: expiry
+        }
+
+        sign_in(@resource)
+        @resource.save!
+
+        yield @resource if block_given?
+
+        redirect_to(@resource.build_auth_url(params[:redirect_url], {
+          token:                        token,
+          client_id:                    client_id,
+          account_confirmation_success: true,
+          config:                       params[:config]
+        }))
+      else
+        raise ActionController::RoutingError.new('Not Found')
+      end
+    end
+  end
+end
